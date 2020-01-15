2e8e636db0289a3be6357bbcdb24c76a05a4bc90
AidenBow <AidenBow@users.noreply.github.com>
2020-01-15 13:52:15 -0800

    journal handle changes

--- src/components/Journal.js
+++ src/components/Journal.js
@@ -2,10 +2,27 @@ import React, {Component} from "react"
 import {connect} from "react-redux"
 
 class Journal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newJournal: "",
+      isLoading: false
+    };
+  }
+
+  handleChanges(event) {
+    this.setState({[event.target.name] : event.target.value})
+    console.log(this.state.newJournal)
+  }
+
   render () {
     return(
       <div>
-        <p>dawdaw</p>
+        <textarea
+          name="newJournal"
+          value= {this.state.newJournal}
+          onChange= {e => this.handleChanges(e)}>
+        </textarea>
       </div>
     )
   }
