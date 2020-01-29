e29c586fa6b3b6ac211ecb6ba815ccd856ed9ea9
AidenBow <AidenBow@users.noreply.github.com>
2020-01-29 00:18:33 -0800

    replaced tab titles with icons

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { Menu, Segment } from 'semantic-ui-react'
 import Journal from "./Journal"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faClock } from '@fortawesome/free-regular-svg-icons'
+import { faBook, faListUl } from '@fortawesome/free-solid-svg-icons'
 
 class Tabs extends Component {
   state = { activeItem: 'Journal' }
@@ -17,22 +20,28 @@ class Tabs extends Component {
             name='Journal'
             active={activeItem === 'Journal'}
             onClick={this.handleItemClick}
-          />
+          >
+            <FontAwesomeIcon icon={faBook} size="2x"/>
+          </Menu.Item>
           <Menu.Item
-            name='Logs'
-            active={activeItem === 'Logs'}
+            name='Log Hours'
+            active={activeItem === 'Log Hours'}
             onClick={this.handleItemClick}
-          />
+          >
+            <FontAwesomeIcon icon={faClock} size="2x"/>
+          </Menu.Item>
           <Menu.Item
             name='Settings'
             active={activeItem === 'Settings'}
             onClick={this.handleItemClick}
-          />
+          >
+            <FontAwesomeIcon icon={faListUl} size="2x"/>
+          </Menu.Item>
 
         </Menu>
         <Segment attached='bottom'>
           {activeItem === "Journal" ?
-            <Journal /> : activeItem === "Logs" ?
+            <Journal /> : activeItem === "Log Hours" ?
             <p>entrys</p> :
             <p>settings</p>
           }
